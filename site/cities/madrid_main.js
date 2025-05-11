ff4b2bca436032d5bcf70df0b4dae7093ba7e15a
Gauthier Nielly <gauthier.nielly@gmail.com>
2025-05-11 16:42:25 +0200

    Restore gitignore, Add webdata folder for JS visu

--- site/cities/madrid_main.js
+++ site/cities/madrid_main.js
@@ -2,7 +2,7 @@ import { drawScatterplot } from "../assets/madrid/madrid_scatter_plot.js";
 import { drawBarplot } from "../assets/madrid/madrid_feature_based_bar_plot.js";
 
 // Load dataset and call visualization functions
-d3.csv("../../data/madrid_final_dataset.csv").then(data => {
+d3.csv("../webdata/madrid_final_dataset.csv").then(data => {
     drawScatterplot(data);
     drawBarplot(data);
 });
