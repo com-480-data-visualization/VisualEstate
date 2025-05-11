06a86f5905e1c0d75dad700d7a4cb1ad3030148a
gauthiernielly <gauthier.nielly@gmail.com>
2025-05-11 15:36:38 +0200

    Update paths to load datasets in berlin_main.js

--- site/cities/berlin_main.js
+++ site/cities/berlin_main.js
@@ -4,8 +4,8 @@ import { drawMap } from "../assets/berlin/berlin_map.js";
 
 // Load datasets and call visualization functions
 Promise.all([
-    d3.json("../../data/berlin_neighborhoods.geojson"),
-    d3.csv("../../data/berlin_final_dataset.csv")
+    d3.json("data/berlin_neighborhoods.geojson"),
+    d3.csv("data/berlin_final_dataset.csv")
 ]).then(([geoData, data]) => {
     drawScatterplot(data);
     drawBarplot(data);
