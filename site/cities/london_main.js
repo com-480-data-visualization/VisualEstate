d30e7e359f1bf38125b3115e7024a2264cda35ce
Gauthier Nielly <gauthier.nielly@gmail.com>
2025-05-11 16:42:25 +0200

    Restore gitignore, Add webdata folder for JS visu

--- site/cities/london_main.js
+++ site/cities/london_main.js
@@ -1,6 +1,6 @@
 import { drawBarplot } from "../assets/london/london_feature_based_bar_plot.js";
 
 // Load dataset and call visualization functions
-d3.csv("../../data/london_houses_dec2024.csv").then(data => {
+d3.csv("../webdata/london_houses_dec2024.csv").then(data => {
     drawBarplot(data);
 });
